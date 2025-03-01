b2b8e5efecdf09b450f2d8178a7bff7c3b195126
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2025-03-01 22:32:09 +1300

    Fixed callPackage import

--- flake.nix
+++ flake.nix
@@ -1,24 +1,24 @@
 {
- description = "Manage secrets over the network";
- inputs = {
-   nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-   systems.url = "github:nix-systems/default";
- };
- outputs = {
-   self,
-   nixpkgs,
-   systems
- }: let
-     eachSystem = nixpkgs.lib.genAttrs (import systems);
- in {
-  nixosModules.netsecrets = ./modules/netsecrets.nix;
-  nixosModules.default = self.nixosModules.netsecrets;
+  description = "Manage secrets over the network";
 
-  overlays.default = import ./overlay.nix;
-  packages = eachSystem (system: {
-	netsecrets = nixpkgs.callPackage ./pkgs/netsecrets.nix {};
-	default = self.packages.${system}.netsecrets;
-  });
- 
- };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, nixpkgs, systems }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    nixosModules.netsecrets = ./modules/netsecrets.nix;
+    nixosModules.default = self.nixosModules.netsecrets;
+
+    overlays.default = import ./overlay.nix;
+
+    packages = eachSystem (system: let
+      pkgs = import nixpkgs { inherit system; }; 
+    in {
+      netsecrets = pkgs.callPackage ./pkgs/netsecrets.nix {};
+      default = self.packages.${system}.netsecrets;
+    });
+  };
 }
