806365344c0cdbcae3f6abf2bd49306dfb7994fc
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2025-04-01 18:31:36 +1300

    Update server.nix

--- modules/netsecrets/server.nix
+++ modules/netsecrets/server.nix
@@ -1,67 +1,64 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.netsecrets.server;
+  netsecrets = import ../lib/default.nix { inherit pkgs; };
+in
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
-  cfg = config.netsecrets;
-  netsecrets = import ../lib/default.nix {inherit pkgs;};
-in {
-  options = {
-    netsecrets = {
-      # NOTE: This is option would be used by the server.
-      #       You can declare the client as a local IP
-      #       in order to request secrets stored locally.
-      server = lib.mkOption {
-        type = lib.types.submodule {
-          options = {
-            enable = lib.mkOption {
-              description = "Whether to enable the netsecrets daemon.";
-              type = lib.types.bool;
-              default = false;
-            };
-            ip = lib.mkOption {
-              description = "IP address for netsecrets server.";
-              type = lib.types.str;
-              default = "";
-            };
-            port = lib.mkOption {
-              description = "Port for netsecrets server.";
-              type = lib.types.str;
-              default = "";
-            };
-            verbose = lib.mkOption {
-              description = "Enable verbose logging for requesting secrets.";
-              type = lib.types.bool;
-              default = false;
-            };
-          };
-        };
+  options.netsecrets.server = {
+    enable = lib.mkEnableOption "the netsecrets daemon";
+
+    ip = lib.mkOption {
+      description = "IP address for netsecrets server";
+      type = lib.types.str;
+      default = "";
+    };
+
+    port = lib.mkOption {
+      description = "Port for netsecrets server";
+      type = lib.types.str;
+      default = "8080";
+    };
+
+    verbose = lib.mkOption {
+      description = "Enable verbose logging for requesting secrets";
+      type = lib.types.bool;
+      default = false;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.mongodb = {
+      enable = true;
+      dbpath = "/var/lib/netsecrets/db";
+      pidFile = "/run/netsecrets/mongodb.pid";
+      user = "netsecrets";
+    };
+
+    systemd.services.netsecrets-daemon = {
+      description = "NetSecrets Daemon";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" "mongodb.service" ];
+      wants = [ "network-online.target" ];
+      serviceConfig = {
+        ExecStart = "${netsecrets.receive}";
+        Restart = "always";
+        User = "root";
+        RuntimeDirectory = "netsecrets";
       };
     };
 
-    config = lib.mkIf cfg.enable {
-      # nosql database for storing secrets
-      services = {
-        mongodb = {
-          enable = true;
-          dbpath = "/var/lib/netsecrets/db";
-          pidFile = "/run/netsecrets/mongodb.pid";
-          user = "netsecrets";
-        };
+    users.users.netsecrets = {
+      isSystemUser = true;
+      group = "netsecrets";
+    };
 
-        netsecrets-daemon = {
-          description = "NetSecrets Daemon";
-          wantedBy = ["multi-user.target"];
-          after = ["network-online.target"];
-          wants = ["network-online.target"];
-          serviceConfig = {
-            ExecStart = netsecrets.receive;
-            Restart = "always";
-            User = "root";
-          };
-        };
-      };
+    users.groups.netsecrets = {};
+
+    environment.etc."netsecrets/config.json".text = builtins.toJSON {
+      ip = cfg.ip;
+      port = cfg.port;
+      verbose = cfg.verbose;
     };
   };
 }
