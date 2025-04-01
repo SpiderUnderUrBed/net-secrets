537e2878dddd722b431e3ce3b7393a2bc9e9ca7a
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2025-04-01 21:07:57 +1300

    Update client.nix

--- modules/netsecrets/client.nix
+++ modules/netsecrets/client.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }:
 
 let
-  cfg = config.netsecrets.client;
   netsecrets = import ../lib/default.nix { inherit pkgs; };
 
   secretType = lib.types.submodule {
@@ -9,11 +8,6 @@ let
       file = lib.mkOption {
         type = lib.types.path;
         description = "Path where the secret will be stored";
-      };
-      value = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
-        default = null;
-        description = "The secret value";
       };
       restartUnits = lib.mkOption {
         type = lib.types.listOf lib.types.str;
@@ -27,39 +21,22 @@ in {
   options.netsecrets.client = {
     enable = lib.mkEnableOption "the netsecrets client";
 
-    requestedSecrets = lib.mkOption {
-      type = lib.types.listOf (lib.types.submodule {
-        options = {
-          name = lib.mkOption {
-            type = lib.types.str;
-            description = "Name of the secret to request";
-          };
-          file = lib.mkOption {
-            type = lib.types.path;
-            default = "/var/lib/netsecrets/secret";
-            description = "Path where the secret will be stored";
-          };
-          restartUnits = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [];
-            description = "Units to restart when this secret changes";
-          };
-        };
-      });
-      default = [];
-      description = "List of secrets to request from server";
+    secrets = lib.mkOption {
+      type = lib.types.attrsOf secretType;
+      default = {};
+      description = "Secrets to fetch from the server";
     };
 
     ip = lib.mkOption {
       type = lib.types.str;
       default = "";
-      description = "IP address for requesting secrets";
+      description = "Server IP address";
     };
 
     port = lib.mkOption {
       type = lib.types.port;
       default = 8080;
-      description = "Port for requesting secrets";
+      description = "Server port";
     };
 
     verbose = lib.mkOption {
@@ -69,13 +46,9 @@ in {
     };
   };
 
-  options.netsecrets.secrets = lib.mkOption {
-    type = lib.types.attrsOf secretType;
-    default = {};
-    description = "Configuration for manually managed secrets";
-  };
-
-  config = lib.mkIf cfg.enable (lib.mkMerge [
+  config = let
+    cfg = config.netsecrets.client;
+  in lib.mkIf cfg.enable (lib.mkMerge [
     {
       system.activationScripts.netsecrets-dir = ''
         mkdir -p /var/lib/netsecrets
@@ -102,33 +75,7 @@ in {
       '';
     }
 
-    # Services for requested secrets
-    (lib.mkMerge (map (secret: {
-      systemd.services."netsecrets-fetch-${secret.name}" = {
-        description = "Fetch secret ${secret.name}";
-        script = ''
-          ${netsecrets.send} get ${secret.name} > ${secret.file}
-          chmod 600 ${secret.file}
-        '';
-        serviceConfig = {
-          Type = "oneshot";
-          User = "root";
-        };
-      };
-      
-    } // lib.optionalAttrs (secret.restartUnits != []) {
-      systemd.services."netsecrets-restart-${secret.name}" = {
-        description = "Restart services for secret ${secret.name}";
-        wantedBy = ["netsecrets-fetch-${secret.name}.service"];
-        script = lib.concatMapStrings (unit: "systemctl try-restart ${unit}\n") secret.restartUnits;
-        serviceConfig = {
-          Type = "oneshot";
-          User = "root";
-        };
-      };
-    }) cfg.requestedSecrets))
-
-    # Services for manually configured secrets
+    # Create services for each secret
     (lib.mapAttrs (name: secret: {
       systemd.services."netsecrets-fetch-${name}" = {
         description = "Fetch secret ${name}";
@@ -141,8 +88,9 @@ in {
           User = "root";
         };
       };
-    }) config.netsecrets.secrets)
+    }) cfg.secrets)
 
+    # Create restart services where needed
     (lib.mapAttrs (name: secret: lib.optionalAttrs (secret.restartUnits != []) {
       systemd.services."netsecrets-restart-${name}" = {
         description = "Restart services for secret ${name}";
@@ -153,6 +101,6 @@ in {
           User = "root";
         };
       };
-    }) config.netsecrets.secrets)
+    }) cfg.secrets)
   ]);
 }
