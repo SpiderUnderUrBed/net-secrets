bc91e66afded7cf46e6017d588b9f7875eba97eb
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2025-04-01 21:45:40 +1300

    Update client.nix

--- modules/netsecrets/client.nix
+++ modules/netsecrets/client.nix
@@ -2,53 +2,42 @@
 
 let
   netsecrets = import ../lib/default.nix { inherit pkgs; };
+  
+  # Function to build secrets attribute set
+  makeSecrets = names: lib.foldl' (acc: name: 
+    acc // { "${name}" = { file = "/var/lib/netsecrets/${name}"; }; }
+  ) {} names;
+
 in {
   options = {
     netsecrets.client = {
       enable = lib.mkEnableOption "the netsecrets client";
-
       secrets = lib.mkOption {
         type = lib.types.listOf lib.types.str;
         default = [];
-        description = "List of secret names to fetch from server";
+        description = "List of secret names to fetch";
       };
-
       ip = lib.mkOption {
         type = lib.types.str;
-        default = "";
-        description = "Server IP address";
-      };
-
-      port = lib.mkOption {
-        type = lib.types.port;
-        default = 8080;
-        description = "Server port";
+        description = "Netsecrets server IP";
       };
     };
 
     secrets = lib.mkOption {
       type = lib.types.attrsOf (lib.types.submodule {
-        options = {
-          file = lib.mkOption {
-            type = lib.types.path;
-            readOnly = true;
-            description = "Path to the secret file";
-          };
+        options.file = lib.mkOption {
+          type = lib.types.path;
+          description = "Path to secret file";
         };
       });
       default = {};
-      description = "Available secrets configuration";
+      internal = true;
     };
   };
 
-  config = let
-    cfg = config.netsecrets.client;
-  in lib.mkIf cfg.enable {
-    # Initialize config.secrets for all requested secrets
-    secrets = lib.listToAttrs (map (name: {
-      name = name;
-      value = { file = "/var/lib/netsecrets/${name}"; };
-    }) cfg.secrets);
+  config = lib.mkIf config.netsecrets.client.enable {
+    # Initialize secrets using foldl
+    secrets = makeSecrets config.netsecrets.client.secrets;
 
     system.activationScripts.netsecrets-dir = ''
       mkdir -p /var/lib/netsecrets
@@ -56,23 +45,17 @@ in {
     '';
 
     systemd.services.netsecrets-client = {
-      description = "NetSecrets Client";
+      description = "Fetch secrets from server";
       wantedBy = ["multi-user.target"];
       after = ["network-online.target"];
-      wants = ["network-online.target"];
       serviceConfig = {
-        ExecStart = pkgs.writeShellScript "netsecrets-fetch" ''
-          set -e
-          ${toString (map (name: 
-            "${netsecrets.send} fetch ${name} --output ${config.secrets.${name}.file}"
-          ) cfg.secrets}
+        ExecStart = let
+          fetchCmd = name: 
+            "${netsecrets.send} fetch ${name} --output ${config.secrets.${name}.file}";
+        in pkgs.writeShellScript "fetch-secrets" ''
+          ${toString (map fetchCmd config.netsecrets.client.secrets)}
         '';
-        Restart = "on-failure";
         User = "root";
-        Environment = [
-          "NETSECRETS_SERVER_IP=${cfg.ip}"
-          "NETSECRETS_SERVER_PORT=${toString cfg.port}"
-        ];
       };
     };
   };
