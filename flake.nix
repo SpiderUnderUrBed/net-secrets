757793a48130819ebe1f809ce14efb5355991ba3
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2025-07-29 19:32:18 +1200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,22 +7,24 @@
 
   outputs = { self, nixpkgs, ... }:
     let
-      systems = [ "x86_64-linux" ];
-      eachSystem = nixpkgs.lib.genAttrs systems;
+      systemList = [ "x86_64-linux" ];
+      eachSystem = nixpkgs.lib.genAttrs systemList;
     in {
+      # Expose per‑system packages.netsecrets and .default
       packages = eachSystem (system:
         let
           pkgs = import nixpkgs { inherit system; };
           netsecretsPkg = pkgs.callPackage ./pkgs/netsecrets.nix {};
         in {
           netsecrets = netsecretsPkg;
-          default = netsecretsPkg;
-        }
-      );
+          default    = netsecretsPkg;
+        });
 
+      # Your NixOS module
       nixosModules.netsecrets = ./modules/netsecrets/default.nix;
-      nixosModules.default = self.nixosModules.netsecrets;
+      nixosModules.default     = self.nixosModules.netsecrets;
 
+      # Any overlay you need
       overlays.default = import ./overlay.nix;
     };
 }
