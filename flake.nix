ee53459ff4df97a095cb1f24e5e87e54d76c3003
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2025-07-29 19:25:09 +1200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,21 +6,25 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, systems, ... }: let
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    nixosModules.netsecrets = ./modules/netsecrets/default.nix;
-    nixosModules.default = self.nixosModules.netsecrets;
+  outputs = { self, nixpkgs, systems, ... }:
+    let
+      # systems is expected to be an attribute set, e.g. { x86_64-linux = ...; aarch64-linux = ...; }
+      eachSystem = nixpkgs.lib.genAttrs (builtins.attrNames systems);
+    in
+    {
+      nixosModules.netsecrets = ./modules/netsecrets/default.nix;
+      nixosModules.default = self.nixosModules.netsecrets;
 
-    overlays.default = import ./overlay.nix;
+      overlays.default = import ./overlay.nix;
 
-    packages = eachSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      netsecretsPkg = pkgs.callPackage ./pkgs/netsecrets.nix {};
-    in {
-      netsecrets = netsecretsPkg;
-      lib = import ./lib {inherit pkgs;};
-      default = netsecretsPkg;
-    });
-  };
+      packages = eachSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          netsecretsPkg = pkgs.callPackage ./pkgs/netsecrets.nix {};
+        in {
+          netsecrets = netsecretsPkg;
+          lib = import ./lib { inherit pkgs; };
+          default = netsecretsPkg;  # No self reference here!
+        });
+    };
 }
